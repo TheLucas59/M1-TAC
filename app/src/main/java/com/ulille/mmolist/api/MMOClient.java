d9eddbaa9b60b4529dbc435ee50c61248cd5e33d
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2022-11-30 16:53:45 +0100

    add retrofit call adapter

--- app/src/main/java/com/ulille/mmolist/api/MMOClient.java
+++ app/src/main/java/com/ulille/mmolist/api/MMOClient.java
@@ -1,6 +1,7 @@
 package com.ulille.mmolist.api;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MMOClient {
@@ -9,7 +10,9 @@ public class MMOClient {
 
     public static Retrofit getRetrofit() {
         if(retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(URL_BASE).addConverterFactory(GsonConverterFactory.create()).build();
+            retrofit = new Retrofit.Builder().baseUrl(URL_BASE)
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create()).build();
         }
         return retrofit;
     }
