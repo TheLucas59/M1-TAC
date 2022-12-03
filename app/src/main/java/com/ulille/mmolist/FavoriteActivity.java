7a2e1ebd3cbe822f67f0e3765045c69425c3a2a2
Aurelien Plancke <aurelien.plancke.etu@univ-lille.fr>
2022-12-03 14:30:04 +0100

    Rename layout for consistency and added flow for navigation in the application

---
+++ app/src/main/java/com/ulille/mmolist/FavoriteActivity.java
@@ -0,0 +1,19 @@
+package com.ulille.mmolist;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FavoriteActivity extends AppCompatActivity {
+    private final String TITLE = "MES FAVORIS";
+    TextView titlePageGames;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.v_all_game);
+        titlePageGames = findViewById(R.id.titlePageGames);
+        titlePageGames.setText(TITLE);
+    }
+}
