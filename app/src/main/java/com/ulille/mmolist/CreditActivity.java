ac477338b770f1cf09f72476b49a442a49741d60
Aurelien Plancke <aurelien.plancke.etu@univ-lille.fr>
2022-12-03 14:30:04 +0100

    Rename layout for consistency and added flow for navigation in the application

---
+++ app/src/main/java/com/ulille/mmolist/CreditActivity.java
@@ -0,0 +1,15 @@
+package com.ulille.mmolist;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.v_credit);
+    }
+}
