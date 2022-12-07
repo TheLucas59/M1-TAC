f482d09933b8a79835cc86dce6fb9faee7c69df9
Aurelien Plancke <aurelien.plancke.etu@univ-lille.fr>
2022-12-07 16:48:30 +0100

    Working recycler view for list

--- app/src/main/java/com/ulille/mmolist/AllGameActivity.java
+++ app/src/main/java/com/ulille/mmolist/AllGameActivity.java
@@ -1,6 +1,7 @@
 package com.ulille.mmolist;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -11,19 +12,29 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ulille.mmolist.api.model.Game;
+import com.ulille.mmolist.viewmodel.GameViewModel;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class AllGameActivity extends AppCompatActivity {
 
     private final int TRANSPARENCY_VAL = 64;
     private final int TRANSPARENCY_BASE = 255;
     RecyclerView recyclerViewAllGames;
-    RecyclerView.Adapter adapterAllGame;
+    GameAdapter adapterAllGame;
     ImageButton buttonGrid;
     ImageButton buttonList;
-    ViewModelProvider viewModelProvider;
+    GameViewModel viewModelGames;
 
     View.OnClickListener onClickGrid = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            recyclerViewAllGames.getAdapter().getItemViewType(R.layout.v_gamecard_grid);
             recyclerViewAllGames.setLayoutManager(
                     new GridLayoutManager(
                             AllGameActivity.this, GridLayoutManager.DEFAULT_SPAN_COUNT));
@@ -40,6 +51,7 @@ public class AllGameActivity extends AppCompatActivity {
     View.OnClickListener onClickList = new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                recyclerViewAllGames.getAdapter().getItemViewType(R.layout.v_gamecard_list);
                 recyclerViewAllGames.setLayoutManager(
                         new LinearLayoutManager(
                                 AllGameActivity.this, LinearLayoutManager.VERTICAL, false));
@@ -55,6 +67,7 @@ public class AllGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.adapterAllGame = new GameAdapter();
         setContentView(R.layout.v_all_game);
         recyclerViewAllGames = findViewById(R.id.recyclerViewAllGames);
 
@@ -62,16 +75,21 @@ public class AllGameActivity extends AppCompatActivity {
         buttonGrid.setOnClickListener(onClickGrid);
         buttonList = findViewById(R.id.buttonList);
         buttonList.setOnClickListener(onClickList);
-
         buttonList.setClickable(false);
         buttonList.setEnabled(false);
         buttonList.getBackground().setAlpha(TRANSPARENCY_VAL);
 
-
-        recyclerViewAllGames.setAdapter(adapterAllGame);
         recyclerViewAllGames.setLayoutManager(new LinearLayoutManager(this));
 
-        //viewModelProvider = new ViewModelProvider(this).get(GameViewModel.class);
+        viewModelGames = new ViewModelProvider(this).get(GameViewModel.class);
+        Observable<List<Game>> observableListGames = viewModelGames.getAllGames();
+        observableListGames.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::subscribeAllGames) ;
+        recyclerViewAllGames.setAdapter(adapterAllGame);
+    }
+
+    private void subscribeAllGames(List<Game> games){
+        this.adapterAllGame.setGames(games);
     }
 
 
