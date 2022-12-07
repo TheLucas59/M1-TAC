1163a6685a556ae1cc19ce4a2f9db940c85f3dd7
Aurelien Plancke <aurelien.plancke.etu@univ-lille.fr>
2022-12-07 15:32:21 +0100

    WIP: all game page with grid and list recycler view

--- app/src/main/java/com/ulille/mmolist/FavoriteActivity.java
+++ app/src/main/java/com/ulille/mmolist/FavoriteActivity.java
@@ -7,7 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FavoriteActivity extends AppCompatActivity {
-    private final String TITLE = "MES FAVORIS";
+    private final String TITLE = getString(R.string.favoriteTitle);
     TextView titlePageGames;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
