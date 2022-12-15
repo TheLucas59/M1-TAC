f389f97729bd058f7e8088ebd33bed181f0bbdaf
Aurelien Plancke <aurelien.plancke.etu@univ-lille.fr>
2022-12-15 13:37:41 +0100

    [Refacto] abstract adapter and viewholder to change between grid and list

--- app/src/main/java/com/ulille/mmolist/activities/CreditActivity.java
+++ app/src/main/java/com/ulille/mmolist/activities/CreditActivity.java
@@ -1,14 +1,17 @@
-package com.ulille.mmolist;
+package com.ulille.mmolist.activities;
 
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RandomActivity extends AppCompatActivity {
+import com.ulille.mmolist.R;
+
+public class CreditActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.v_detail_jeu);
+        setContentView(R.layout.v_credit);
     }
 }
