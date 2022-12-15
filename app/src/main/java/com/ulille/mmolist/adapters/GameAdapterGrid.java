4d051c5e43f178360befc2c26bcfd2404ace3e87
Aurelien Plancke <aurelien.plancke.etu@univ-lille.fr>
2022-12-15 13:37:41 +0100

    [Refacto] abstract adapter and viewholder to change between grid and list

---
+++ app/src/main/java/com/ulille/mmolist/adapters/GameAdapterGrid.java
@@ -0,0 +1,30 @@
+package com.ulille.mmolist.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.ulille.mmolist.R;
+import com.ulille.mmolist.viewholders.GameViewHolderGrid;
+
+public class GameAdapterGrid extends AbstractGameAdapter<GameViewHolderGrid>{
+    public GameAdapterGrid(Context context) {
+        super(context);
+    }
+
+    @NonNull
+    @Override
+    public GameViewHolderGrid onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int layout = R.layout.v_gamecard_grid;
+        View view = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        return new GameViewHolderGrid(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameViewHolderGrid holder, int position) {
+        super.onBindViewHolder(holder, position);
+    }
+}
